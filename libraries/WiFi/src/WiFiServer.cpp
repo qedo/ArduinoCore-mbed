aa91ecd234aa49a4c2c2daa1276eae0cdff9cf18
Andreas Donner <qedo@me.com>
2020-10-23 22:51:56 +0200

    Update WiFiServer.cpp
    added: sock->set_blocking(false);

--- libraries/WiFi/src/WiFiServer.cpp
+++ libraries/WiFi/src/WiFiServer.cpp
@@ -35,6 +35,7 @@ size_t arduino::WiFiServer::write(const uint8_t *buf, size_t size) {
 arduino::WiFiClient arduino::WiFiServer::available(uint8_t* status) {
 	WiFiClient client;
 	nsapi_error_t error;
+    sock->set_blocking(false);
 	TCPSocket* clientSocket = sock->accept(&error);
 	if(status != nullptr) {
 		*status = error == NSAPI_ERROR_OK ? 1 : 0;
